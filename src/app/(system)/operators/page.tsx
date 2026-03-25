28c701f9e8a2592c524c3958982bd06dd3a161de
Carlos Daniel Simoes dos Santos <cdaniel@MKMONDIAL.local>
2026-03-25 10:53:29 -0400

    Feat: Ajustei erro no vercel, prop antigo do operators

--- src/app/(system)/operators/page.tsx
+++ src/app/(system)/operators/page.tsx
@@ -42,7 +42,6 @@ export default function OperatorsPage() {
           workstations={table.workstations}
           onRemove={table.handleRemoveOperator}
           onChangeLine={table.handleChangePosition}
-          onChangePosto={table.handleChangePosition}
         />
       </div>
 
